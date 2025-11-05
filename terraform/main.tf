57a91668a6092e70e4ed3facc60fd11e0fdaca92
Ahmad Amiraslanli (Data AnalyticsAamiraslanliAI) <aamiraslanli@azercell.com>
2025-11-05 20:49:40 +0400

    added sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws.manager+sandbox@azercell.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws.manager+sandbox@azercell.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
